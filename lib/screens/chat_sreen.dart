c03aa08e5904b2de2a61ca204d3e5b8e21ce90d9
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-17 15:52:49 +0800

    Rendering Stream Data with StreamBuilder

--- lib/screens/chat_sreen.dart
+++ lib/screens/chat_sreen.dart
@@ -1,32 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
 class ChatScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, i) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("Works!"),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/CFzzAHPzaJJg6RzWveNi/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final documents = streamSnapshot.data.documents;
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () async {
-          await Firebase.initializeApp();
-          return FirebaseFirestore.instance
-              .collection('chats/CFzzAHPzaJJg6RzWveNi/messages')
-              .snapshots()
-              .listen((data) {
-            // print(data.docs[0]['text']);
-            data.docs.forEach((document) {
-              print(document['text']);
-            });
-          });
+        onPressed: () {
+          // await Firebase.initializeApp();
+          // return FirebaseFirestore.instance
+          //     .collection('chats/CFzzAHPzaJJg6RzWveNi/messages')
+          //     .snapshots()
+          //     .listen((data) {
+          //   // print(data.docs[0]['text']);
+          //   data.docs.forEach((document) {
+          //     print(document['text']);
+          //   });
+          // });
         },
       ),
     );
