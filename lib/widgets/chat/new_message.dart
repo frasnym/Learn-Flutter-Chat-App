a64e32017a91b01843bf1a964fc155216955517e
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-20 14:10:45 +0800

    Creating a Basic Chat Bubble Widget

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -16,6 +16,7 @@ class _NewMessageState extends State<NewMessage> {
     FirebaseFirestore.instance.collection('chat').add({
       'text': _enteredMessage,
       'createdAt': Timestamp.now(),
+      // 'userId': 
     });
 
     _controller.clear();
