21f3238b0273a3abd3cc390b5bee0feffc7751f8
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-21 10:11:15 +0800

    Displaying User Images

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -21,6 +21,7 @@ class _NewMessageState extends State<NewMessage> {
       'createdAt': Timestamp.now(),
       'userId': userId,
       'username': userData['username'],
+      'userImage': userData['image_url'],
     });
 
     _controller.clear();
